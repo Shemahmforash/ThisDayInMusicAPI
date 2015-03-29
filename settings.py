7bbb2ba6987ab542159223f5f9a8e02102c15ff0
Carlos Rosão <shemahmforash@hotmail.com>
2015-03-29 16:48:24 +0000

    First steps in defining schema for the main endpoints

---
+++ settings.py
@@ -0,0 +1,67 @@
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'event': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+   '': {
+        'type': 'list',
+        'allowed': ["author", "contributor", "copy"],
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'}
+        },
+    },
+    'date': {
+        'type': 'datetime',
+    },
+}
+
+event = {
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform
+    # GET requests at '/people/<lastname>'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'date'
+    },
+
+    # We choose to override global cache-control directives for this resource.
+    'cache_control': 'max-age=10,must-revalidate',
+    'cache_expires': 10,
+
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+
+    'schema': schema
+}
+
+DOMAIN = {
+    'event': event,
+}
+
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
