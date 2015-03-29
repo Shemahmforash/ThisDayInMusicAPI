d5f8057652ddc33097b35d21c40c5d35a293f17a
Carlos Rosão <shemahmforash@hotmail.com>
2015-03-29 17:15:46 +0000

    Simplified api schema

--- settings.py
+++ settings.py
@@ -1,32 +1,13 @@
 schema = {
     # Schema definition, based on Cerberus grammar. Check the Cerberus project
     # (https://github.com/nicolaiarocci/cerberus) for details.
-    'event': {
+    'description': {
         'type': 'string',
-        'minlength': 1,
-        'maxlength': 10,
+        'minlength': 5,
     },
-    'lastname': {
-        'type': 'string',
-        'minlength': 1,
-        'maxlength': 15,
-        'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-   '': {
+   'type': {
         'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
+        'allowed': ["Event", "Birth", "Death"],
     },
     'date': {
         'type': 'datetime',
@@ -34,22 +15,6 @@ schema = {
 }
 
 event = {
-    # by default the standard item entry point is defined as
-    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
-    # additional read-only entry point. This way consumers can also perform
-    # GET requests at '/people/<lastname>'.
-    'additional_lookup': {
-        'url': 'regex("[\w]+")',
-        'field': 'date'
-    },
-
-    # We choose to override global cache-control directives for this resource.
-    'cache_control': 'max-age=10,must-revalidate',
-    'cache_expires': 10,
-
-    # most global settings can be overridden at resource level
-    'resource_methods': ['GET', 'POST'],
-
     'schema': schema
 }
 
